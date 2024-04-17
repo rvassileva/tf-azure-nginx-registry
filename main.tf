bfe3d50701d037e6e493faba79e1f6b4f42b8a13
rvassileva <136706933+rvassileva@users.noreply.github.com>
2024-04-17 10:15:01 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ source = "gitlab.com/rvasileva/terraform-azurerm-networking.git?ref=v0.0.1"
 module "compute" {
   # source                = "app.terraform.io/Test-Roza/compute/azurerm"
   # version               = "0.0.1"
-  source = "bitbucket.org/rozasworkspace/terraform-azurerm-compute?ref=v0.0.1"
+  source = "bitbucket.org/rozasworkspace/terraform-azurerm-compute.git?ref=v0.0.1"
   location              = module.networking.location
   rg_name               = module.networking.websrv_rg
   instance_name         = var.instance_name
